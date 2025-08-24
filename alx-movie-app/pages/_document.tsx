d1531940e3ee9a2ce788dd2b5c7a02e05a47e3b0
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-08-24 08:02:35 +0300

    Web App Manifest

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link the PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color (used for browser UI) */}
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* Fallback icons for older browsers */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
